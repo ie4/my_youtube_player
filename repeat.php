beec9c5ed1780a2c73f5d49890f8fe75e9a38388
ie4 <kei@ie4.me>
2015-07-01 02:37:22 +0900

    rebuild with YouTube IFrame API

--- repeat.php
+++ repeat.php
@@ -1,11 +1,11 @@
 <?php
+// https://developers.google.com/youtube/iframe_api_reference?hl=ja
+// https://developers.google.com/youtube/player_parameters?hl=ja
 if($_GET["pl"]){
   $pl = $_GET["pl"];
-  $prms = "?listType=playlist&list=$pl";
 }
 if($_GET["v"]){
   $v = $_GET["v"] ;
-  $prms = "$v?loop=1&playlist=$v";
 }
 ?>
 <html>
@@ -18,15 +18,47 @@ body{
 </style>
 </head>
 <body align=center>
-<iframe width="320" height="240" src="//www.youtube.com/embed/<?php print $prms; ?>" frameborder="0" allowfullscreen></iframe>
-<!--
-https://developers.google.com/youtube/iframe_api_reference?hl=ja
-https://developers.google.com/youtube/player_parameters?hl=ja
--->
+<div id="player"></div>
+<script>
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '240',
+      width: '320',
+      events: {
+        'onReady': onPlayerReady
+      }
+    });
+  }
+  function onPlayerReady(event) {
+    event.target.loadPlaylist(<?php echo ($pl) ? "{list:'$pl'}" : "'$v'" ; ?>);
+    event.target.setLoop(true);
+  }
+  function prevVideo() {
+    player.previousVideo();
+  }
+  function nextVideo() {
+    player.nextVideo();
+  }
+  function loopVideo() {
+    list = player.getPlaylist();
+    document.getElementById("v").value = list[player.getPlaylistIndex()];
+    document.getElementById("pl").value = '';
+    document.forms[0].submit();
+  }
+</script>
 <form action="repeat.php">
-<input type="text" name="v" value="<?php print $v; ?>" size="10">
-<input type="text" name="pl" value="<?php print $pl; ?>" size="12">
+<a href="javascript:void(0);" onClick="prevVideo()" style="color:#fff;text-decoration:none;">&lt;&lt;</a>
+<input type="text" id="v" name="v" value="<?php print $v; ?>" size="8">
+<a href="javascript:void(0);" onClick="loopVideo()" style="color:#fff;text-decoration:none;"> o </a>
+<input type="text" id="pl" name="pl" value="<?php print $pl; ?>" size="8">
 <input type="submit">
+<a href="javascript:void(0);" onClick="nextVideo()" style="color:#fff;text-decoration:none;">&gt;&gt;</a>
 </form>
 </body>
 </html>
