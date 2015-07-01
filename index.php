d8178159d9658b1640ce01bbbd38d84b37a0104d
ie4 <kei@ie4.me>
2015-07-02 01:14:45 +0900

    add localStorage

--- index.php
+++ index.php
@@ -13,14 +13,46 @@ function openRepeatPlayer(){
   var videoId    = document.getElementById('video_id').value;
   var playlistId = document.getElementById('playlist_id').value;
   window.open('repeat.php?v='+videoId+'&pl='+playlistId, windowName, windowOption);
+  if(videoId)    addLocalStorage("videoId",videoId) ;
+  if(playlistId) addLocalStorage("playlistId",playlistId) ;
 }
 function openVideoIdsGetPlayer(){
   var url = escape(document.getElementById('url').value);
   window.open('video_ids_geter.php?url='+url, windowName, windowOption);
+  if(url) addLocalStorage("url",url) ;
 }
 function openSearchPlayer(){
   var query = document.getElementById('search_query').value;
   window.open('search.php?q='+query, windowName, windowOption);
+  if(query) addLocalStorage("search_query",query) ;
+}
+function addLocalStorage(name,value){
+  var hash = JSON.parse(localStorage.getItem(name));
+  if(hash === null){
+    hash = {};
+    hash[value] = 1;
+  }else if(hash[value] === undefined){
+    hash[value] = 1;
+  }else{
+    hash[value] ++;
+  }
+  localStorage.setItem(name,JSON.stringify(hash));
+}
+window.onload = function(){
+  var names = ['videoId','playlistId','url','search_query'];
+  var historyText = "";
+  for(var i=0; i < names.length; i++ ){
+    var name = names[i];
+    var list = JSON.parse(localStorage.getItem(name));
+    historyText += "========== " + name + "\n" ;
+    for(var key in list){
+      historyText += key + " : " + list[key] + "\n" ;
+    }
+  }
+  var pre = document.createElement( "pre" );
+  pre.style.cssText = "line-height:1.8em";
+  pre.innerHTML = historyText;
+  document.body.appendChild( pre );
 }
 </script>
 <form>
